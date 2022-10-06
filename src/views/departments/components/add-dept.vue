6da7b5279944fb08336fa990a1b7bc2ce500a1b3
wangjianyun <wjy131088@gmail.com>
2022-10-06 10:14:57 +0800

    feat:部门新增完成

--- src/views/departments/components/add-dept.vue
+++ src/views/departments/components/add-dept.vue
@@ -11,8 +11,8 @@
         <el-input v-model="formData.code" style="width:80%" placeholder="1-50个字符" />
       </el-form-item>
       <el-form-item prop="manager" label="部门负责人">
-        <el-select v-model="formData.manager" style="width:80%" placeholder="请选择">
-          <el-option label="username11" value="username" />
+        <el-select v-model="formData.manager" style="width:80%" placeholder="请选择" @focus="getEmployeeSimple">
+          <el-option v-for="item in peoples" :key="item.id" :label="item.username" :value="item.username" />
         </el-select>
       </el-form-item>
       <el-form-item prop="introduce" label="部门介绍">
@@ -27,14 +27,15 @@
     <el-row slot="footer" type="flex" justify="center">
       <!-- 列被分为24 -->
       <el-col :span="6">
-        <el-button type="primary" size="small">确定</el-button>
-        <el-button size="small">取消</el-button>
+        <el-button v-loading="loading" type="primary" size="small" @click="submit">确定</el-button>
+        <el-button size="small" @click="handleClose">取消</el-button>
       </el-col>
     </el-row>
   </el-dialog>
 </template>
 <script>
-import { getDepartments } from '@/api/departments'
+import { getDepartments, addDepartments } from '@/api/departments'
+import { getEmployeeSimple } from '@/api/employees'
 export default {
   props: {
     dialogVisible: {
@@ -62,7 +63,7 @@ export default {
       // 如何去找技术部所有的子节点
       const deptstj = depts.filter(item => item.pid === this.treeNode.id)
       const isRepeat = deptstj.some(ele => ele.name === value)
-      isRepeat ? callback(new Error(`该部门下一届存在${value}部门名称`)) : callback()
+      isRepeat ? callback(new Error(`该部门下一级存在${value}部门名称`)) : callback()
     }
     return {
       formData: {
@@ -89,13 +90,39 @@ export default {
           { required: true, message: '部门介绍必填', trigger: 'blur' },
           { min: 1, max: 300, message: '部门介绍1-300个字符', trigger: 'blur' }
         ]
-      }
+      },
+      peoples: [], // 接收获取的员工简单列表的数据
+      loading: false
     }
   },
   methods: {
     handleClose() {
       this.$emit('update:dialogVisible', false)
       this.$refs.addDeptForm.resetFields()
+      this.form = {
+        name: '', // 部门名称
+        code: '', // 部门编码
+        manager: '', // 部门管理者
+        introduce: '' // 部门介绍
+      }
+    },
+    // 获取员工简单列表数据
+    async getEmployeeSimple() {
+      this.peoples = await getEmployeeSimple()
+    },
+    async submit() {
+      try {
+        await this.$refs.addDeptForm.validate()
+        this.loading = true
+        await addDepartments({ ...this.formData, pid: this.treeNode.id }) // 调用新增接口 添加父部门的id
+        this.$message.success('新增成功')
+        this.$parent.getDepartments() // 告诉父组件，刷新列表
+        this.handleClose()
+      } catch (e) {
+        console.log(e)
+      } finally {
+        this.loading = false
+      }
     }
   }
 }
